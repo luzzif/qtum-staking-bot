83490d52ecef9b82eda4072f1deb9fcbdac7f303
luzzif <federico.luzzi@mondora.com>
2019-10-14 14:37:19 +0200

    feat: fake server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "@babel/polyfill";
 import { bot } from "./bot";
 import { handleGetRichness } from "./commands/get-richness";
+import express from "express";
 
 bot.command("getRichness", handleGetRichness);
 bot.launch();
+express().listen(8080);
