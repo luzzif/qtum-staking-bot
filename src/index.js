a7983e04aebcfed53ca1ce00b07695534dceac85
luzzif <federico.luzzi@mondora.com>
2019-10-16 15:40:25 +0200

    feat: listen to specific port

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { bot } from "./bot";
 import { handleGetRichness } from "./commands/get-richness";
 import express from "express";
 import fetch from "node-fetch";
-import { APP_URL } from "./env";
+import { APP_URL, PORT } from "./env";
 
 bot.command("getRichness", handleGetRichness);
 bot.startPolling();
 
-express().get("/", (request, response) => response.status(204).send());
+express()
+    .get("/", (request, response) => response.status(204).send())
+    .listen(PORT);
 
 setInterval(() => {
     fetch(APP_URL);
