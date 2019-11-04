69f57e72679b7471e1c49f69277e20be897015b1
Paolo Guerra <guerra.cico@hotmail.it>
2019-11-04 23:52:21 +0100

    refactor: remove lodash dep

--- src/commands/get-last-stake/index.js
+++ src/commands/get-last-stake/index.js
@@ -1,23 +1,23 @@
 import { map } from "bluebird";
-import { flatten, compact } from "lodash";
 import { DateTime } from "luxon";
 import { ADDRESSES } from "../../env"
 import { qtumApiClient } from "../../clients/qtum-api"
 
 export const handleGetLastStake = async context => {
     try {
-        const transactions = flatten(await map(ADDRESSES, async address => {
+        const transactionLists = await map(ADDRESSES, async address => {
             // We assume that the latest stake tx is within the last 10 txs
             const { data } = await qtumApiClient.get(`/address/${address}/txs?limit=10&offset=0`);
             return data.transactions;
-        }));
+        });
+        const transactions = [].concat(...transactionLists);
 
-        const stakeTxsTimestamps = compact(await map(transactions, async tx => {
+        const stakeTxsTimestamps = await map(transactions, async tx => {
             const { data } = await qtumApiClient.get(`/tx/${tx}`);
-            if (data.isCoinstake === true) {
+            if (data.isCoinstake) {
                 return data.timestamp;
             }
-        }));
+        }).filter(tx => tx);
 
         const lastStakeDate = DateTime.fromSeconds(stakeTxsTimestamps[0]).toFormat("dd/LL/yyyy");
         return context.replyWithMarkdown(
