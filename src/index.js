9165fbe8e6401c01beb7df31c92bb03359d1ad6b
luzzif <federico.luzzi@mondora.com>
2019-11-26 12:52:01 +0100

    chore: move all code inside handler to have better error handling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const {
-    handleGetLastValidatedBlock
-} = require("./commands/get-last-validated-block");
-const { handleGetRichness } = require("./commands/get-richness");
-const Telegraf = require("telegraf");
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-const botUsername = process.env.BOT_USERNAME;
-bot.command(["gr", `gr@${botUsername}`], handleGetRichness);
-bot.command(["glvb", `glvb@${botUsername}`], handleGetLastValidatedBlock);
-
 exports.handler = (event, context, callback) => {
     try {
+        const {
+            handleGetLastValidatedBlock
+        } = require("./commands/get-last-validated-block");
+        const { handleGetRichness } = require("./commands/get-richness");
+
+        const bot = new require("telegraf")(process.env.BOT_TOKEN);
+        const botUsername = process.env.BOT_USERNAME;
+        bot.command(["gr", `gr@${botUsername}`], handleGetRichness);
+        bot.command(
+            ["glvb", `glvb@${botUsername}`],
+            handleGetLastValidatedBlock
+        );
+
         bot.handleUpdate(JSON.parse(event.body));
         return callback(null, { statusCode: 204 });
     } catch (error) {
