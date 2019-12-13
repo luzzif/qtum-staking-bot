943c0bdacce638a91cc9c9177bd7d3f71f226712
luzzif <federico.luzzi@mondora.com>
2019-12-13 21:55:00 +0100

    fix: correctly handle response

--- src/commands/get-staking-status/index.js
+++ src/commands/get-staking-status/index.js
@@ -4,35 +4,42 @@ const jayson = require("jayson");
 exports.handleGetStakingStatus = async context => {
     try {
         const client = jayson.client.http(`http://${process.env.NODE_IP}:3888`);
-        const response = await client.request("getstakinginfo", []);
-        console.log("Dude", response);
-        const {
-            enabled,
-            staking,
-            weight,
-            expectedtime: expectedTime
-        } = response;
-        const messages = [];
-        if (!enabled) {
-            messages.push("Staking currently *not enabledé");
-        } else {
+        await client.request("getstakinginfo", [], (error, response) => {
+            if (error) {
+                console.error("error getting staking status", error);
+                return context.replyWithMarkdown(
+                    "An error occurred, please try again later..."
+                );
+            }
+            const {
+                enabled,
+                staking,
+                weight,
+                expectedtime: expectedTime
+            } = response.result;
+            console.log(response);
+            const messages = [];
+            if (!enabled) {
+                messages.push("Staking currently *not enabledé");
+            } else {
+                messages.push(
+                    staking
+                        ? "Staking currently enabled and *in progressé"
+                        : "Staking currently enabled and *not in progress*"
+                );
+            }
+            messages.push(`Current network weight: *${weight}*`);
             messages.push(
-                staking
-                    ? "Staking currently enabled and *in progressé"
-                    : "Staking currently enabled and *not in progress*"
+                `Expected time until next block: *${new Decimal(expectedTime)
+                    .dividedBy(60)
+                    .dividedBy(60)
+                    .dividedBy(24)
+                    .toFixed(2)}*`
             );
-        }
-        messages.push(`Current network weight: *${weight}*`);
-        messages.push(
-            `Expected time until next block: *${new Decimal(expectedTime)
-                .dividedBy(60)
-                .dividedBy(60)
-                .dividedBy(24)
-                .toFixed(2)}*`
-        );
-        return context.replyWithMarkdown(messages.join("\n- "));
+            return context.replyWithMarkdown(messages.join("\n- "));
+        });
     } catch (error) {
-        console.error("error getting richness", error);
+        console.error("error getting staking status", error);
         return context.replyWithMarkdown(
             "An error occurred, please try again later..."
         );
