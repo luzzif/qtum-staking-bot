be2656d723b3d9dc02109e1d44fb3818f4caf46c
luzzif <federico.luzzi@mondora.com>
2019-12-24 19:18:49 +0100

    feat: get staking info refactor

--- src/commands/get-staking-status/index.js
+++ src/commands/get-staking-status/index.js
@@ -1,43 +1,47 @@
 const { Decimal } = require("decimal.js");
-const jayson = require("jayson");
+const fetch = require("node-fetch");
+const uuid = require("uuid");
 
 exports.handleGetStakingStatus = async context => {
     try {
-        const client = jayson.client.http(`http://${process.env.NODE_IP}:3888`);
-        await client.request("getstakinginfo", [], (error, response) => {
-            if (error) {
-                console.error("error getting staking status", error);
-                return context.replyWithMarkdown(
-                    "An error occurred, please try again later..."
-                );
+        const response = await fetch(`https://${process.env.NODE_IP}:3889`, {
+            method: "POST",
+            body: JSON.stringify({
+                jsonrpc: "1.0",
+                id: uuid.v4(),
+                method: "getstakinginfo",
+                params: []
+            }),
+            headers: {
+                Authorization: `Basic ${Buffer.from(
+                    `${process.env.RPC_USER}:${process.env.RPC_PASSWORD}`
+                ).toString("base64")}`
             }
-            const {
-                enabled,
-                staking,
-                weight,
-                expectedtime: expectedTime
-            } = response.result;
-            console.log(response);
-            const messages = [];
-            if (!enabled) {
-                messages.push("Staking currently *not enabledé");
-            } else {
-                messages.push(
-                    staking
-                        ? "Staking currently enabled and *in progressé"
-                        : "Staking currently enabled and *not in progress*"
-                );
-            }
-            messages.push(`Current network weight: *${weight}*`);
-            messages.push(
-                `Expected time until next block: *${new Decimal(expectedTime)
-                    .dividedBy(60)
-                    .dividedBy(60)
-                    .dividedBy(24)
-                    .toFixed(2)}*`
-            );
-            return context.replyWithMarkdown(messages.join("\n- "));
         });
+        if (!response.ok) {
+            throw new Error();
+        }
+        const { result } = await response.json();
+        const { enabled, staking, weight, expectedtime: expectedTime } = result;
+        const messages = [];
+        if (!enabled) {
+            messages.push("Staking currently *not enabledé");
+        } else {
+            messages.push(
+                staking
+                    ? "Staking currently enabled and *in progressé"
+                    : "Staking currently enabled and *not in progress*"
+            );
+        }
+        messages.push(`Current network weight: *${weight}*`);
+        messages.push(
+            `Expected time until next block: *${new Decimal(expectedTime)
+                .dividedBy(60)
+                .dividedBy(60)
+                .dividedBy(24)
+                .toFixed(2)}*`
+        );
+        return context.replyWithMarkdown(messages.join("\n- "));
     } catch (error) {
         console.error("error getting staking status", error);
         return context.replyWithMarkdown(
